b49bb4659c554143af37a7255ac69d220770d64e
Csbennett22 <90526059+Csbennett22@users.noreply.github.com>
2022-02-02 22:09:18 -0700

    X

---
+++ DateMe/DateMe/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using DateMe.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+namespace DateMe.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private DateApplicationContext daContext { get; set; }
+
+        public HomeController(DateApplicationContext someName)
+        {
+            daContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Movies ()
+        {
+            ViewBag.Majors = daContext.Majors.ToList();
+            return View("MovieApp");
+        }
+        [HttpPost]
+        public IActionResult Movies(ApplicationResponse ar)
+        {
+            if(ModelState.IsValid)
+            {
+                daContext.Add(ar);
+                daContext.SaveChanges();
+                return View("Confirmation", ar); // pass in the bundled up ar
+            }
+            else
+            {
+                ViewBag.Majors = daContext.Majors.ToList();
+
+                return View("MovieApp");
+            }
+        }
+        [HttpGet]
+        public IActionResult MovieList()
+        {
+            var applications = daContext.Responses
+                .Include(x => x.Major) // also include the major object
+                .Where(x => x.Stalker == false) //c# uses double = sign
+                .OrderBy(x => x.LastName) // filtering the responses that are returned to the website
+                .ToList();
+            return View("List", applications);
+        }
+        [HttpGet]
+        public IActionResult Edit(int applicationid) // this name has to match what the view has for asp-route- ________
+        {
+            ViewBag.Majors = daContext.Majors.ToList();
+            
+            var application = daContext.Responses.Single(x => x.ApplicationId == applicationid); // find the single record where the app id is the same as one of the ones in the database
+            
+            return View("DatingApp", application);
+        }
+        [HttpPost]
+        public IActionResult Edit(ApplicationResponse ar)
+        {
+            daContext.Update(ar);
+            daContext.SaveChanges();
+            
+            return RedirectToAction("Waitlist"); // makes it so we don't have to load up all of the same information for that action
+        }
+        [HttpGet]
+        public IActionResult Delete(int applicationid)
+        {
+
+            var application = daContext.Responses.Single(x => x.ApplicationId == applicationid);
+
+            return View(application);
+        }
+        [HttpPost]
+        public IActionResult Delete(ApplicationResponse ar)
+        {
+            daContext.Responses.Remove(ar);
+            daContext.SaveChanges();
+            return RedirectToAction("Waitlist");
+        }
+    }
+}
---
+++ DateMe/DateMe/Models/DateApplicationContext.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateMe.Models
+{
+    public class DateApplicationContext : DbContext // inheritance
+    {
+        //Constructor
+       public DateApplicationContext(DbContextOptions<DateApplicationContext> options) : base (options)
+        {
+            //leave blank
+        }
+
+        public DbSet<ApplicationResponse> Responses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+        // seeding the data
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+
+            mb.Entity<Category>().HasData(
+                    new Category { CategoryId = 1, CategoryName = "Action / Adventure" },
+                    new Category { CategoryId = 2, CategoryName = "Comedy" },
+                    new Category { CategoryId = 3, CategoryName = "Drama" },
+                    new Category { CategoryId = 4, CategoryName = "Family" },
+                    new Category { CategoryId = 5, CategoryName = "Horror/Suspense" },
+                    new Category { CategoryId = 6, CategoryName = "Miscellaneous" },
+                    new Category { CategoryId = 7, CategoryName = "Television" },
+                    new Category { CategoryId = 8, CategoryName = "VHS" }
+
+                );
+            mb.Entity<ApplicationResponse>().HasData(
+
+                new ApplicationResponse
+                {
+                    ApplicationID = 1,
+                    CategoryId = 1,
+                    Title = "Avengers, The",
+                    Year = 2012,
+                    Director = "Joss Whedon",
+                    Rating = "PG-13",
+
+
+                },
+                new ApplicationResponse
+                {
+                    ApplicationID = 2,
+                    CategoryId = 1,
+                    Title = "Batman",
+                    Year = 1989,
+                    Director = "Tim Burton",
+                    Rating = "PG-13",
+                },
+                new ApplicationResponse
+                {
+                    ApplicationID = 3,
+                    CategoryId = 1,
+                    Title = "Dark Knight, The",
+                    Year = 2008,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13",
+                }
+                );
+        }
+    }
+
+}
